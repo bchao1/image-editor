a4e9c169f508e087600536344e914fc3ed27c549
bchao1 <whereisbrianchao520@gmail.com>
2020-08-19 19:10:20 +0800

    add sharing button

--- app/__init__.py
+++ app/__init__.py
@@ -10,11 +10,13 @@ from PIL import Image
 
 from .api import api as api_blueprint
 from .errors import add_error_handlers
-from .utils import serve_pil_image
+from .utils import serve_pil_image, b64ToImage
 
 from .imaging.filters import filter_dict
 from .imaging.enhance import enhance_dict
 from .imaging.quantize import quantize_dict
+
+import json 
 
 def create_app():
     app = Flask(__name__, static_url_path='', 
@@ -64,6 +66,25 @@ def recieve_multiple_files():
         
         return serve_pil_image(img, file_extention), 200
 
+@application.route("/shareimage", methods=["GET", "POST"])
+def recieve_share_image():
+    # Save (key, value) = (img id, img data) pair to binary store
+    b64str = request.values.get('image_data').split("base64,")[-1]
+    img_id = request.values.get('image_id')
+    print("Recieved image ", img_id)
+    img = b64ToImage(b64str)  # return PIL image
+    return "Share Success!", 200
+
+@application.route("/undo-op", methods=["POST"])
+def undo_operation():
+    sessID = request.values.get("sess")
+    return "Undo op", 200
+
+@application.route("/redo-op", methods=["POST"])
+def redo_operation():
+    sessID = request.values.get("sess")
+    return "Redo op", 200
+
 @application.route("/uploadsingle", methods=["GET", "POST"])
 def recieve_single_file():
     """ Recieve single uploaded file from client
@@ -85,6 +106,7 @@ def recieve_single_file():
     file_extention = uploaded_file.filename.split('.')[-1]  # get file extension
     print('File received', uploaded_file.filename)
     print('File extension', file_extention)
+    img_data = request.get
 
     with Image.open(uploaded_file.stream) as img:
         # process PIL image (plugin processing functions here)
