db0baf6f80a24dccb1b5f4ce338b4799320e8b9e
bchao1 <whereisbrianchao520@gmail.com>
2020-08-28 21:22:04 +0800

    fix alignment in color_filter __init__.py

--- app/imaging/color_filters/__init__.py
+++ app/imaging/color_filters/__init__.py
@@ -12,14 +12,14 @@ def warm_filter(img):
     pix = img_warm.load()
     
     for i in range(width):
-            for j in range(height):
-                a = pix[i, j][0]
-                b = pix[i, j][1]
-                c = pix[i, j][2]
-                a -= b // 4 
-                b -= c // 4 
-                c -= a // 4
-                draw.point((i, j), (a, b, c))
+        for j in range(height):
+            a = pix[i, j][0]
+            b = pix[i, j][1]
+            c = pix[i, j][2]
+            a -= b // 4 
+            b -= c // 4 
+            c -= a // 4
+            draw.point((i, j), (a, b, c))
                 
     return img_warm
     
@@ -33,34 +33,33 @@ def old_filter(img):
     pix = img_old.load()
     
     for i in range(width):
-            for j in range(height):
-                a = pix[i, j][0]
-                b = pix[i, j][1]
-                c = pix[i, j][2]
-                red = a
-                green = b
-                blue = c
-                s = red + green + blue
+        for j in range(height):
+            a = pix[i, j][0]
+            b = pix[i, j][1]
+            c = pix[i, j][2]
+            red = a
+            green = b
+            blue = c
+            s = red + green + blue
+            
+            a = a - s // 6 + blue // 10 
+            b = b - s // 5 + red // 9
+            c = c - s // 6
+            
+            s = a + b + c
+            if (s > 300):
+                a += 7
+                b += 7
+                c += 7
+            else :
+                a += 5
+                b += 5
+                c += 6
                 
-                a = a - s // 6 + blue // 10 
-                b = b - s // 5 + red // 9
-                c = c - s // 6
+            if (a > 100) :
+                a -= a // 10
                 
-                s = a + b + c
-                if (s > 300):
-                    a += 7
-                    b += 7
-                    c += 7
-                else :
-                    a += 5
-                    b += 5
-                    c += 6
-                    
-                if (a > 100) :
-                    a -= a // 10
-                    
-                draw.point((i, j), (a, b, c))
-
+            draw.point((i, j), (a, b, c))
     return img_old
     
     
@@ -73,28 +72,28 @@ def green_filter(img):
     pix = img_green.load()
     
     for i in range(width):
-            for j in range(height):
-                a = pix[i, j][0]
-                b = pix[i, j][1]
-                c = pix[i, j][2]
-                a -= b // 3
-                b -= b // 8
-                c -= a // 6
+        for j in range(height):
+            a = pix[i, j][0]
+            b = pix[i, j][1]
+            c = pix[i, j][2]
+            a -= b // 3
+            b -= b // 8
+            c -= a // 6
+            
+            if (b > 100):
+                b -= b//8
+            else:
+                b -= b//11
                 
-                if (b > 100):
-                    b -= b//8
-                else:
-                    b -= b//11
-                    
-                if (a + c < 100):
-                    a += a // 10
-                    c += c // 10
-                else:
-                    a += a // 18
-                    c += c // 22
-                    
-                draw.point((i, j), (a, b, c))
-    
+            if (a + c < 100):
+                a += a // 10
+                c += c // 10
+            else:
+                a += a // 18
+                c += c // 22
+                
+            draw.point((i, j), (a, b, c))
+
     return img_green
     
 def noise1(img):
